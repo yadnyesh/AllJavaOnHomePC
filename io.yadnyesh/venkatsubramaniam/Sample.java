3b050d3956aa01f1694d59c001aa5c1b4836ba87
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-12-19 18:18:40 +0530

    Imperative Style of coding

--- io.yadnyesh/venkatsubramaniam/Sample.java
+++ io.yadnyesh/venkatsubramaniam/Sample.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class Sample {
 
-    List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        int result = 0;
+        for(int e : numbers){
+            if(e % 2 ==0){
+                result += e * 2;
+            }
+        }
+    }
 
 }
